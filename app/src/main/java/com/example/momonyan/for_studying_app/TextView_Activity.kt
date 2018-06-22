fb74ad2bcb5cc3be2e45c2aa63f799ac73ddee84
Bluewind12 <zero18030@gmail.com>
2018-06-22 13:19:35 +0900

    Main_screen_end

---
+++ app/src/main/java/com/example/momonyan/for_studying_app/TextView_Activity.kt
@@ -0,0 +1,13 @@
+//テキストビュー(TextViewの表示)
+
+package com.example.momonyan.for_studying_app
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class TextView_Activity:AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.textview_layout)
+    }
+}
