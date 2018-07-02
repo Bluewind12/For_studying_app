f68fa5ba2ced7c7cc2fbaa5871f3fcd69326da97
Bluewind12 <zero18030@gmail.com>
2018-07-02 11:59:32 +0900

    Sound_Framework_End

--- app/src/main/java/com/example/momonyan/for_studying_app/Sound_Activity.kt
+++ app/src/main/java/com/example/momonyan/for_studying_app/Sound_Activity.kt
@@ -1,7 +1,30 @@
 package com.example.momonyan.for_studying_app
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 
-class Sound_Activity :AppCompatActivity(){
+class Sound_Activity : AppCompatActivity() {
+    private lateinit var questionButton: Button
+    private lateinit var collectButton: Button
+    private lateinit var unCollectButton: Button
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sound_layout)
+
+        questionButton = findViewById(R.id.So_button)
+        collectButton = findViewById(R.id.So_button2)
+        unCollectButton = findViewById(R.id.So_button3)
+
+        questionButton.setOnClickListener {
+
+        }
+        collectButton.setOnClickListener {
+
+        }
+        unCollectButton.setOnClickListener {
+
+        }
+    }
 }
