9a29b0b056fee650aae46565fa8323c6c1db5502
Bluewind12 <zero18030@gmail.com>
2018-07-18 19:23:56 +0900

    Motion correction # 音楽再生の動作修正 + 音楽がなるようになりました。

--- app/src/main/java/com/example/momonyan/for_studying_app/Sound_Activity.kt
+++ app/src/main/java/com/example/momonyan/for_studying_app/Sound_Activity.kt
@@ -19,15 +19,7 @@ class Sound_Activity : AppCompatActivity() {
     //音データは app/res/raw(作る)の中に入れて使う
 
     //サウンドプール用の宣言・SEに適している
-    val soundPool = SoundPool.Builder().build()
-    val sound1 = soundPool.load(this, R.raw.question1, 1)
-    val sound2 = soundPool.load(this, R.raw.correct1, 1)
-    val sound3 = soundPool.load(this, R.raw.incorrect1, 1)
-
-    //メディアプレイヤー用の宣言・BGMに適している(Webから音源を持ってくることも可能)
-    val mediaPlayerBGM = MediaPlayer.create(getApplicationContext(),R.raw.bgm)
-    //再生中かの判別
-    private var isPlaying = false
+    private lateinit var soundPool: SoundPool
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +30,15 @@ class Sound_Activity : AppCompatActivity() {
         collectButton = findViewById(R.id.So_button2)
         unCollectButton = findViewById(R.id.So_button3)
         bgmButton = findViewById(R.id.So_buttonBGM)
+        soundPool = SoundPool.Builder().build()
+
+        val sound1 = soundPool.load(this, R.raw.question1, 1)
+        val sound2 = soundPool.load(this, R.raw.correct1, 1)
+        val sound3 = soundPool.load(this, R.raw.incorrect1, 1)
+
+        //メディアプレイヤー用の宣言・BGMに適している(Webから音源を持ってくることも可能)
+        val mediaPlayerBGM = MediaPlayer.create(getApplicationContext(), R.raw.bgm)
+        mediaPlayerBGM.isLooping = true
 
         questionButton.setOnClickListener {
             soundPool.play(sound1, 1.0f, 1.0f, 0, 0, 1.0f)
@@ -50,15 +51,12 @@ class Sound_Activity : AppCompatActivity() {
         }
 
         bgmButton.setOnClickListener {
-            if (isPlaying) {
-                mediaPlayerBGM.stop()
-                mediaPlayerBGM.prepare()
+            if (mediaPlayerBGM.isPlaying) {
+                mediaPlayerBGM.pause()
                 bgmButton.text = "BGM再生"
-                isPlaying = false
             } else {
                 mediaPlayerBGM.start()
                 bgmButton.text = "BGM停止"
-                isPlaying = true
             }
         }
     }
